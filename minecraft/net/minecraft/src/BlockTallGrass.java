933c16d214bbb8ecb3c9945d6166143b28045992
ByteReport <ByteReportHolder@gmail.com>
2025-09-12 17:21:38 +1000

    Visual improvements to tall grass

--- minecraft/net/minecraft/src/BlockTallGrass.java
+++ minecraft/net/minecraft/src/BlockTallGrass.java
@@ -13,21 +13,28 @@ public class BlockTallGrass extends BlockFlower {
 		return i2 == 1 ? this.blockIndexInTexture : (i2 == 2 ? this.blockIndexInTexture + 16 + 1 : (i2 == 0 ? this.blockIndexInTexture + 16 : this.blockIndexInTexture));
 	}
 
+//	public int colorMultiplier(IBlockAccess iBlockAccess1, int i2, int i3, int i4) {
+//		int i5 = iBlockAccess1.getBlockMetadata(i2, i3, i4);
+//		if(i5 == 0) {
+//			return 0xFFFFFF;
+//		} else {
+//			long j6 = (long)(i2 * 3129871 + i4 * 6129781 + i3);
+//			j6 = j6 * j6 * 42317861L + j6 * 11L;
+//			i2 = (int)((long)i2 + (j6 >> 14 & 31L));
+//			i3 = (int)((long)i3 + (j6 >> 19 & 31L));
+//			i4 = (int)((long)i4 + (j6 >> 24 & 31L));
+//			iBlockAccess1.getWorldChunkManager().func_4069_a(i2, i4, 1, 1);
+//			double d8 = iBlockAccess1.getWorldChunkManager().temperature[0];
+//			double d10 = iBlockAccess1.getWorldChunkManager().humidity[0];
+//			return ColorizerGrass.getGrassColor(d8, d10);
+//		}
+//	}
+
 	public int colorMultiplier(IBlockAccess iBlockAccess1, int i2, int i3, int i4) {
-		int i5 = iBlockAccess1.getBlockMetadata(i2, i3, i4);
-		if(i5 == 0) {
-			return 0xFFFFFF;
-		} else {
-			long j6 = (long)(i2 * 3129871 + i4 * 6129781 + i3);
-			j6 = j6 * j6 * 42317861L + j6 * 11L;
-			i2 = (int)((long)i2 + (j6 >> 14 & 31L));
-			i3 = (int)((long)i3 + (j6 >> 19 & 31L));
-			i4 = (int)((long)i4 + (j6 >> 24 & 31L));
-			iBlockAccess1.getWorldChunkManager().func_4069_a(i2, i4, 1, 1);
-			double d8 = iBlockAccess1.getWorldChunkManager().temperature[0];
-			double d10 = iBlockAccess1.getWorldChunkManager().humidity[0];
-			return ColorizerGrass.getGrassColor(d8, d10);
-		}
+		iBlockAccess1.getWorldChunkManager().func_4069_a(i2, i4, 1, 1);
+		double d5 = iBlockAccess1.getWorldChunkManager().temperature[0];
+		double d7 = iBlockAccess1.getWorldChunkManager().humidity[0];
+		return ColorizerGrass.getGrassColor(d5, d7);
 	}
 
 	public int idDropped(int i1, Random random2) {
